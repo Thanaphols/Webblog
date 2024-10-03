24f0bf6a9f9924659f8b0dce9798e0f8eb1794de
Nier <cupteo254504@gmail.com>
2024-10-03 21:30:43 +0700

    Front

---
+++ req/footer.php
@@ -0,0 +1,16 @@
+<style>
+.footer {
+  
+  background-color: #F0A04B;
+}
+</style>
+<!-- Footer -->
+<footer class="text-center footer text-white mt-auto ">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="">
+      © TEAM :
+      <a class="text-white" href="index.php">WatchBlog</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
